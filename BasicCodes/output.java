2e16389c2a6fa20d3d8d0c6fbc29098d1aa24f50
Anamika12113 <anamikagupta1910@gmail.com>
2026-01-06 13:41:32 +0530

    added my.java

---
+++ BasicCodes/output.java
@@ -0,0 +1,8 @@
+public class output {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        System.out.println("To");
+        System.out.println("Hello World");
+    }
+    
+}
